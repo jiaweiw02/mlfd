b13ad691b95481a176c0af94710159f575dd112e
Jiawei Wu <wuj22@rpi.edu>
2023-10-08 23:24:57 -0400

    handwritting

--- hw6/handwriting/handwriting.py
+++ hw6/handwriting/handwriting.py
@@ -1,3 +1,7 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
 def readTrain():
     train = open("ZipDigits.train", 'r')
     allDigits = []
@@ -12,4 +16,25 @@ def readTrain():
     return allDigits
 
 
-readTrain()
+def create_grayscale_image(data):
+    # Create a 16x16 grid
+    grid_size = 16
+    grid = np.zeros((grid_size, grid_size))
+
+    # Map the data to grayscale values
+    for i in range(grid_size):
+        for j in range(grid_size):
+            index = i * grid_size + j
+            value = data[index]
+            grid[i, j] = value
+
+    # Display the grayscale image
+    plt.imshow(grid, cmap='gray')
+    plt.axis('off')  # Turn off axis labels and ticks
+    plt.show()
+
+
+five_val = readTrain()[14][1:]
+one_val = readTrain()[1][1:]
+create_grayscale_image(five_val)
+create_grayscale_image(one_val)
