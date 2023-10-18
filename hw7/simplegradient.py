8673c18adcb065ed502b5418e190c6f4e0106d92
jiaweiw02 <wuj22@rpi.edu>
2023-10-18 17:19:07 -0400

    hw7

--- hw7/simplegradient.py
+++ hw7/simplegradient.py
@@ -14,25 +14,42 @@ def gradientY(x, y):
     return 4 * y + 4 * np.pi * np.sin(2 * np.pi * x) * np.cos(2 * np.pi * y)
 
 
-x = 0.1
-y = 0.1
+pointsX = [0.1, 1, -0.5, -1]
+pointsY = [0.1, 1, -0.5, -1]
+minLoc = []
+minValue = []
+
 learningR = 0.1
 iterations = 50
+for j in range(4):
+    x, y = pointsX[j], pointsY[j]
+    xVal = []
+    yVal = []
+    currMinLoc = None
+    currMinVal = np.inf
+    for i in range(iterations):
+        func = f(x, y)
 
-xVal = []
-yVal = []
+        gX = gradientX(x, y)
+        gY = gradientY(x, y)
 
-for i in range(iterations):
-    func = f(x, y)
+        x -= learningR * gX
+        y -= learningR * gY
 
-    gX = gradientX(x, y)
-    gY = gradientY(x, y)
+        xVal.append(i)
+        yVal.append(func)
+        if func < currMinVal:
+            currMinLoc = x, y
+            currMinVal = func
+    minLoc.append(currMinLoc)
+    minValue.append(currMinVal)
+    plt.plot(xVal, yVal, label="({},{})".format(pointsX[j], pointsY[j]))
 
-    x -= learningR * gX
-    y -= learningR * gY
-
-    xVal.append(i)
-    yVal.append(func)
-
-plt.plot(xVal, yVal)
+print("x start & y start & x min & y min & min value\\\\")
+for i in range(4):
+    print("{} & {} & {} & {} & {}\\\\".format(pointsX[i], pointsY[i], minLoc[i][0], minLoc[i][1], minValue[i]))
+print(minLoc, minValue)
+plt.xlabel("iterations")
+plt.ylabel("f(x, y)")
+plt.legend()
 plt.show()
