dbf2375033a35f6f121c408305d5e3233c32bc5b
jiaweiw02 <wuj22@rpi.edu>
2023-12-03 20:32:16 -0500

    i have no idea what im doing

--- hw12/code/main.py
+++ hw12/code/main.py
@@ -1,20 +1,46 @@
 import numpy as np
+from hw9 import Dtrain_Dtest
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 def p1():
-    m = 2
-    initWeights = [[0.15 for _ in range(3)] for _ in range(2)]  # 3 x 2
-    x = [2, 1]
-    y = -1
+    x = np.array([[2], [1]])
+    y = np.array([[-1]])
+    w1 = np.full((2, 2), 0.1501)
+    w2 = np.full((2, 1), 0.1501)
 
-    x1 = [1]
-    # runs only two times
-    for l in range(1, m + 1):
-        s = np.dot(np.transpose(initWeights), x)
-        x = np.tanh(s)
-        print(x)
-        initWeights = [0.15 for _ in range(m + 1)]
+    # tanh
+    s1 = np.dot(w1.T, x)
+    x1 = np.tanh(s1)
+
+    # identity
+    s2 = np.dot(w2.T, x1)
+    x2 = s2
+
+    # error, one point
+    Ein = (1 / 4) * np.sum((x2 - y) ** 2)
+
+    delta2Identity = 2 * (x2 - y)
+    w2Identity = (1 / 2) * np.dot(x1, delta2Identity.T)
+    delta2TanH = 2 * (np.tanh(s2) - y) * 1 - np.tanh(s2) ** 2
+    w2TanH = (1 / 2) * np.dot(x1, delta2TanH.T)
+
+    delta1Identity = np.dot(w2, delta2Identity) * (1 - np.tanh(s1) ** 2)
+    w1Identity = (1 / 2) * np.dot(x, delta1Identity.T)
+    delta1TanH = np.dot(w2, delta2TanH) * (1 - np.tanh(s1) ** 2)
+    w1TanH = (1 / 2) * np.dot(x, delta1TanH.T)
+
+    print("identity:\nw1: {}\nw2: {}".format(w1Identity, w2Identity))
+    print("tanh:\nw1: {}\nw2: {}".format(w1TanH, w2TanH))
+    print(Ein)
 
 
 if "__main__" == __name__:
     p1()
+    file = "ZipDigits.all"
+    data = Dtrain_Dtest(file)
+    trainingData = data[0]
+    testingData = data[1]
